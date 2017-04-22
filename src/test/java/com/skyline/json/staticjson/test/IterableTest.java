deb9f6774207d04081f6a4e68481b0b90c68a2a8
陈亮 <hzchenliang@corp.netease.com>
2017-04-22 16:13:56 +0800

    change: 提交Iterable测试用例

--- src/test/java/com/skyline/json/staticjson/test/IterableTest.java
+++ src/test/java/com/skyline/json/staticjson/test/IterableTest.java
@@ -16,9 +16,8 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.concurrent.ArrayBlockingQueue;
 
 /**
  * Created by chenliang on 2017/4/22.
@@ -51,7 +50,7 @@ public class IterableTest {
 
         ArrayList<Double> doubleArrayList;
 
-        Set<Item> itemSet;
+        Queue<Item> itemQueue;
 
     }
 
@@ -61,6 +60,14 @@ public class IterableTest {
 
         String val2;
 
+        public Item(boolean val1, String val2) {
+            this.val1 = val1;
+            this.val2 = val2;
+        }
+
+        private Item() {
+
+        }
     }
 
     /**
@@ -95,5 +102,93 @@ public class IterableTest {
         Assert.assertEquals(json, json2);
     }
 
+    @Test
+    public void test2() throws IOException {
+        TestClass t = new TestClass();
 
+        t.characterSet = new HashSet<>();
+        for (int i = 0; i < 6; i++) {
+            t.characterSet.add((char) ('a' + i));
+        }
+        String json1 = gson.toJson(t);
+        System.out.println(json1);
+        String json2 = staticJsonConverter.convert2Json(t);
+        System.out.println(json2);
+        Assert.assertEquals(json1, json2);
+    }
+
+    @Test
+    public void test21() throws IOException {
+        testDeserialization("{\"characterSet\":[\"a\",\"b\",\"c\",\"d\",\"e\",\"f\"]}");
+    }
+
+    public void testDeserialization(String json) throws IOException {
+        TestClass t1 = (TestClass) staticJsonConverter.convert2Object(json);
+        TestClass t2 = gson.fromJson(json, TestClass.class);
+        String json1 = staticJsonConverter.convert2Json(t1);
+        System.out.println(json1);
+        String json2 = gson.toJson(t2);
+        System.out.println(json2);
+        Assert.assertEquals(json, json1);
+        Assert.assertEquals(json, json2);
+    }
+
+    @Test
+    public void test3() throws IOException {
+        TestClass t = new TestClass();
+
+        t.integerList = new LinkedList<>();
+        for (int i = 0; i < 6; i++) {
+            t.integerList.add(i);
+        }
+        String json1 = gson.toJson(t);
+        System.out.println(json1);
+        String json2 = staticJsonConverter.convert2Json(t);
+        System.out.println(json2);
+        Assert.assertEquals(json1, json2);
+    }
+
+    @Test
+    public void test31() throws IOException {
+        testDeserialization("{\"integerList\":[0,1,2,3,4,5]}");
+    }
+
+
+    @Test
+    public void test4() throws IOException {
+        TestClass t = new TestClass();
+        t.doubleArrayList = new ArrayList<>();
+        for (int i = 0; i < 6; i++) {
+            t.doubleArrayList.add(i + (double) i / 10);
+        }
+        String json1 = gson.toJson(t);
+        System.out.println(json1);
+        String json2 = staticJsonConverter.convert2Json(t);
+        System.out.println(json2);
+        Assert.assertEquals(json1, json2);
+    }
+
+    @Test
+    public void test41() throws IOException {
+        testDeserialization("{\"doubleArrayList\":[0.0,1.1,2.2,3.3,4.4,5.5]}");
+    }
+
+    @Test
+    public void test5() throws IOException {
+        TestClass t = new TestClass();
+        t.itemQueue = new ArrayBlockingQueue<Item>(100);
+        for (int i = 0; i < 6; i++) {
+            t.itemQueue.add(new Item(i % 2 == 0, "str" + i));
+        }
+        String json1 = gson.toJson(t);
+        System.out.println(json1);
+        String json2 = staticJsonConverter.convert2Json(t);
+        System.out.println(json2);
+        Assert.assertEquals(json1, json2);
+    }
+
+    @Test
+    public void test51() throws IOException {
+        testDeserialization("{\"itemQueue\":[{\"val1\":true,\"val2\":\"str0\"},{\"val1\":false,\"val2\":\"str1\"},{\"val1\":true,\"val2\":\"str2\"},{\"val1\":false,\"val2\":\"str3\"},{\"val1\":true,\"val2\":\"str4\"},{\"val1\":false,\"val2\":\"str5\"}]}");
+    }
 }
